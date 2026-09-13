f95dd45722a87dbc58918bfde9a2efdb2fc79d1c
Sayp <officialsayp@proton.me>
2026-09-13 19:13:29 +0300

    fix CI workflows and handler error handling

--- order-service/handlers/go.go
+++ order-service/handlers/go.go
@@ -34,7 +34,7 @@ func getOrderHandler(w http.ResponseWriter, r *http.Request) {
 
 	detailsStr := r.URL.Query().Get("details")
 	details, err2 := strconv.ParseBool(detailsStr)
-	if err2 != nil && detailsStr == "" {
+	if err2 != nil && detailsStr != "" {
 		http.Error(w, "Некорректный параметр details", http.StatusBadRequest)
 		return
 	}
@@ -71,7 +71,7 @@ func createOrderHandler(orderService *service.OrderService) http.HandlerFunc {
 			return
 		}
 		err := orderService.CreateOrder(req.Product)
-		if err != nil && errors.Is(err, ErrProductUnavailable) {
+		if err != nil && errors.Is(err, service.ErrProductUnavailable) {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
